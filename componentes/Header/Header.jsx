d51fd80221995d77a45b87288781cad91c17ab77
dlopes14 <donylopes@gmail.com>
2022-08-25 13:14:30 -0300

    pull main

--- componentes/Header/Header.jsx
+++ componentes/Header/Header.jsx
@@ -1,22 +1,32 @@
 import React from "react";
+import { sanitize } from "../sanitize";
 
 import style from "./Header.module.css";
 
 const Header = ({
     titulo,
-    tituloDestaque,
     texto,
     botao,
     chamada
 }) => {
   return (
     <div className={style.conteinerHeader}>
-        <div className={style.tituloHeader}>{titulo}<span className={style.tituloDestaqueHeader}>{tituloDestaque}</span></div>
-        <div className={style.textoHeader}>{texto}</div>
-        <div className={style.conteinerChamadasHeader}>
+        <div 
+            className={style.tituloHeader}
+            dangerouslySetInnerHTML={{
+                __html: sanitize(titulo),
+              }}            
+        ></div>
+        <div 
+            className={style.textoHeader}
+            dangerouslySetInnerHTML={{
+                __html: sanitize(texto),
+              }}            
+            ></div>
+        {(botao.label || chamada.label) && <div className={style.conteinerChamadasHeader}>
             {botao.label && <a className={style.buttonHeader} href={botao.url}>{botao.label.toUpperCase()}</a>}
             {chamada.label && <a className={style.consultoriaHeader} href={chamada.url}>{chamada.label.toUpperCase()}</a>}
-        </div>
+        </div>}
     </div>
 )};
 
--- componentes/index.js
+++ componentes/index.js
@@ -1,16 +1,16 @@
 export { ButtonBar } from "./ButtonBar/ButtonBar";
-export { ButtonLight} from "./ButtonLight/ButtonLight";
+export { ButtonLight } from "./ButtonLight/ButtonLight";
 export { ButtonLogin } from "./ButtonLogin/ButtonLogin";
 export { Content3Col } from "./Content3Col/Content3Col";
 export { ConteudoBlog } from "./ConteudoBlog/ConteudoBlog";
 export { Footer } from "./Footer/Footer";
 export { FormConsultoria } from "./FormConsultoria/FormConsultoria";
-export { Header } from "./Header";
+export { Header } from "./Header/Header";
 export { HomeBanner } from "./HomeBanner/HomeBanner";
 export { IFrame}  from "./IFrame/IFrame"
 export { ImagemFundo } from "./ImagemFundo/ImagemFundo";
 export { NavBar } from "./NavBar/NavBar";
-export { Parcerias } from "./Parcerias";
+export { Parcerias } from "./Parcerias/Parcerias";
 export { ParceriasTexto } from "./ParceriasTexto/ParceriasTexto";
 export { Slider } from "./Slider/Slider";
 export { TextCol } from "./TextCol/TextCol";
