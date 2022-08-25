cbe5a552165571c1b1285cafce0cd54f51665dd0
dlopes14 <donylopes@gmail.com>
2022-08-25 17:55:05 -0300

    Componente ArtigosRelacionados Boilerplate

---
+++ componentes/ArtigosRelacionados/ArtigosRelacionados.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ArtigosRelacionados } from './index'
+
+
+export default {
+  title: "Componentes/ArtigosRelacionados",
+  component: ArtigosRelacionados
+}
+
+export const Default = () => {
+  return <ArtigosRelacionados/>
+
+}
---
+++ componentes/ArtigosRelacionados/index.js
@@ -0,0 +1,1 @@
+export { ArtigosRelacionados } from "./ArtigosRelacionados";
