79e432196957a11cda5ba3fcddc5607ec413fcbe
dlopes14 <donylopes@gmail.com>
2022-08-25 18:21:49 -0300

    update index

--- componentes/index.js
+++ componentes/index.js
@@ -9,9 +9,12 @@ export { Header } from "./Header/Header";
 export { HomeBanner } from "./HomeBanner/HomeBanner";
 export { IFrame}  from "./IFrame/IFrame"
 export { ImagemFundo } from "./ImagemFundo/ImagemFundo";
+export { ListaArtigos } from "./ListaArtigos/ListaArtigos";
 export { NavBar } from "./NavBar/NavBar";
 export { Parcerias } from "./Parcerias/Parcerias";
 export { ParceriasTexto } from "./ParceriasTexto/ParceriasTexto";
+export { PreviewArtigo } from "./PreviewArtigo/PreviewArtigo";
+export { PreviewArtigoDestaque } from "./PreviewArtigoDestaque/PreviewArtigoDestaque";
 export { Slider } from "./Slider/Slider";
 export { TextCol } from "./TextCol/TextCol";
 export { TituloTexto } from "./TituloTexto/TituloTexto";
