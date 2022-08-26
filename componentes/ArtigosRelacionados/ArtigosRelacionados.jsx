fff982b34a9a67498fb1b6eeefd37d68fd53fe2f
vinnidias <vinicius.3991@gmail.com>
2022-08-25 23:43:43 -0300

    componente de artigo relacionado

--- componentes/ArtigosRelacionados/ArtigosRelacionados.jsx
+++ componentes/ArtigosRelacionados/ArtigosRelacionados.jsx
@@ -1,9 +1,27 @@
 import React from "react";
-import style from "./ArtigosRelacionados.module.css"
+import style from "./ArtigosRelacionados.module.css";
+import {PreviewArtigo} from "../PreviewArtigo"
 
-const ArtigosRelacionados = ()=>{
+const ArtigosRelacionados = ({list})=>{
     return(
-        <></>
+        <div className={style.ArtigosRelacionadosContainer}>
+        <header>
+          <h1>Artigos Relacionados</h1>
+        </header>
+  
+        <section>
+          {list.map((item, index) => (
+            <PreviewArtigo
+              key={index}
+              tag={item.tag}
+              titulo={item.titulo}
+              texto={item.texto}
+              autor={{ nome: item.autor, avatar: item.avatar, data: item.data }}
+              imagem={item.imagem}
+            />
+          ))}
+        </section>
+      </div>
     )
 }
 
--- componentes/ArtigosRelacionados/ArtigosRelacionados.stories.js
+++ componentes/ArtigosRelacionados/ArtigosRelacionados.stories.js
@@ -2,12 +2,106 @@ import React from 'react'
 import { ArtigosRelacionados } from './index'
 
 
+export const artigos = [
+	{
+			id: "cl6v73chm2xbn0blwom0s8o1l",
+			tag : "Capitação Ponderada",
+			titulo : "1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			texto :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat velit nec erat aliquam fringilla. Nam vestibulum eu neque non convallis. Pellentesque elementum dignissim ante. Nulla lacinia enim lacinia erat cursus scelerisque erata nam vestibulum eu neque non convallis.",
+			autor : "IMPULSO GOV",
+			avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w",  
+			data:  "2022-01-24T00:42:00+00:00",
+			imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
+	},
+	{
+			id: "cl6v73chm2xbn0blwom0s8o1",
+			tag : "Capitação Ponderada",
+			titulo : "2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			texto :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat velit nec erat aliquam fringilla. Nam vestibulum eu neque non convallis. Pellentesque elementum dignissim ante. Nulla lacinia enim lacinia erat cursus scelerisque erata nam vestibulum eu neque non convallis.",
+			autor : "IMPULSO GOV",
+			avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w",  
+			data:  "2022-02-24T00:42:00+00:00",
+			imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
+	},
+	{
+			id: "cl6v73chm2xbn0blwom0s8o",
+			tag : "Capitação Ponderada",
+			titulo : "3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			texto :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat velit nec erat aliquam fringilla. Nam vestibulum eu neque non convallis. Pellentesque elementum dignissim ante. Nulla lacinia enim lacinia erat cursus scelerisque erata nam vestibulum eu neque non convallis.",
+			autor : "IMPULSO GOV",
+			avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w",  
+			data:  "2022-03-24T00:42:00+00:00",
+			imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
+	},
+	{
+			id: "cl6v73chm2xbn0blwom0s8",
+			tag : "Capitação Ponderada",
+			titulo : "4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			texto :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat velit nec erat aliquam fringilla. Nam vestibulum eu neque non convallis. Pellentesque elementum dignissim ante. Nulla lacinia enim lacinia erat cursus scelerisque erata nam vestibulum eu neque non convallis.",
+			autor : "IMPULSO GOV",
+			avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w",  
+			data:  "2022-04-24T00:42:00+00:00",
+			imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
+	},
+	{
+			id: "cl6v73chm2xbn0blwom0s",
+			tag : "Capitação Ponderada",
+			titulo : "5 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			texto :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat velit nec erat aliquam fringilla. Nam vestibulum eu neque non convallis. Pellentesque elementum dignissim ante. Nulla lacinia enim lacinia erat cursus scelerisque erata nam vestibulum eu neque non convallis.",
+			autor : "IMPULSO GOV",
+			avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w",  
+			data:  "2022-05-24T00:42:00+00:00",
+			imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
+	},
+	{
+			id: "cl6v73chm2xbn0blwom0",
+			tag : "Capitação Ponderada",
+			titulo : "6 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			texto :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat velit nec erat aliquam fringilla. Nam vestibulum eu neque non convallis. Pellentesque elementum dignissim ante. Nulla lacinia enim lacinia erat cursus scelerisque erata nam vestibulum eu neque non convallis.",
+			autor : "IMPULSO GOV",
+			avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w",  
+			data:  "2022-06-24T00:42:00+00:00",
+			imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
+	},
+	{
+			id: "cl6v73chm2xbn0blwom",
+			tag : "Capitação Ponderada",
+			titulo : "7 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			texto :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat velit nec erat aliquam fringilla. Nam vestibulum eu neque non convallis. Pellentesque elementum dignissim ante. Nulla lacinia enim lacinia erat cursus scelerisque erata nam vestibulum eu neque non convallis.",
+			autor : "IMPULSO GOV",
+			avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w",  
+			data:  "2022-07-24T00:42:00+00:00",
+			imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
+	},
+	{
+			id: "cl6v73chm2xbn0blwo",
+			tag : "Capitação Ponderada",
+			titulo : "8 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			texto :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat velit nec erat aliquam fringilla. Nam vestibulum eu neque non convallis. Pellentesque elementum dignissim ante. Nulla lacinia enim lacinia erat cursus scelerisque erata nam vestibulum eu neque non convallis.",
+			autor : "IMPULSO GOV",
+			avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w",  
+			data:  "2022-09-24T00:42:00+00:00",
+			imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
+	},
+	{
+			id: "cl6v73chm2xbn0blw",
+			tag : "Capitação Ponderada",
+			titulo : "9 - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+			texto :"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat velit nec erat aliquam fringilla. Nam vestibulum eu neque non convallis. Pellentesque elementum dignissim ante. Nulla lacinia enim lacinia erat cursus scelerisque erata nam vestibulum eu neque non convallis.",
+			autor : "IMPULSO GOV",
+			avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w",  
+			data:  "2022-08-24T00:42:00+00:00",
+			imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
+	},
+]
+
+
 export default {
   title: "Componentes/ArtigosRelacionados",
   component: ArtigosRelacionados
 }
 
 export const Default = () => {
-  return <ArtigosRelacionados/>
+  return <ArtigosRelacionados list={artigos}/>
 
 }
