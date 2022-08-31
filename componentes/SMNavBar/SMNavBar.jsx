1e5879bbf1654e3d7bb68e1685b5beb096ae8727
vinnidias <vinicius.3991@gmail.com>
2022-08-30 22:13:09 -0300

    SM Nav Bar Component first steps

---
+++ componentes/SMNavBar/SMNavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./SMNavBar.module.css"
+
+const SMNavBar = () => {
+  return(
+    <div className={style.SMNavBarContainer}>
+      <span>Olá Mundo</span>
+    </div>
+  )
+}
+
+export {SMNavBar}
---
+++ componentes/SMNavBar/SMNavBar.stories.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { SMNavBar } from "./index"
+
+export default {
+  title: "Componentes/SMNavBar",
+  component: SMNavBar,
+}
+
+
+export const Default = () =>{
+return <SMNavBar/>
+}
+
