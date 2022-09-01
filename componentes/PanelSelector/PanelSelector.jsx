401a0e8ef1b5e4d24991eebec78e090f707ac3fc
vinnidias <vinicius.3991@gmail.com>
2022-08-31 21:24:44 -0300

    componente refatorado para atender wireframe e responsividade

--- componentes/PanelSelector/PanelSelector.jsx
+++ componentes/PanelSelector/PanelSelector.jsx
@@ -1,25 +1,38 @@
 import React, { useState } from "react";
+import { IFrame } from "../IFrame/IFrame";
 import style from "./PanelSelector.module.css";
 
-const PanelSelector = ({ list }) => {
+const PanelSelector = (props) => {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const [iframeLink, setInframeLink] = useState(props.list[0].link);
+
+  console.log(iframeLink);
 
   return (
-    <div className={style.PanelSelectorContainer}>
-      {list.map((item, index) => (
-        <buton
-          onClick={() => setActiveTabIndex(index)}
-          key={index}
-          className={
-            activeTabIndex === index
-              ? style.PanelSelectorButtonSelected
-              : style.PanelSelectorButton
-          }
-        >
-          {item}
-        </buton>
-      ))}
-    </div>
+    <>
+      <div className={style.PanelSelectorMain}>
+        <p>{props.title}</p>
+        <div className={style.PanelSelectorContainer}>
+          {props.list.map((item, index) => (
+            <buton
+              onClick={() => {
+                setActiveTabIndex(index);
+                setInframeLink(item.link);
+              }}
+              key={index}
+              className={
+                activeTabIndex === index
+                  ? style.PanelSelectorButtonSelected
+                  : style.PanelSelectorButton
+              }
+            >
+              {item.label}
+            </buton>
+          ))}
+        </div>
+      </div>
+      <IFrame link={iframeLink} />
+    </>
   );
 };
 
