0cb52925efe5a2e8c2594798f675c6bcd5df5bc4
vinnidias <vinicius.3991@gmail.com>
2022-08-31 21:25:09 -0300

    ajuste de props para funcionamento do componente

--- componentes/PanelSelector/PanelSelector.stories.js
+++ componentes/PanelSelector/PanelSelector.stories.js
@@ -2,20 +2,47 @@ import React from "react";
 import { PanelSelector } from "./index";
 
 const tabs = [
-  "RESUMO",
-  "APS E CAPS",
-  "APS E AMBULATÓRIO",
-  "RAPS E ATENÇÃO HOSPITALAR",
-];
+  {
+    label: "RESUMO",
+    link: "https://www.delftstack.com/pt/api/",
+  },
+  {
+    label: "APS E CAPS",
+    link: "https://www.delftstack.com/pt/tutorial/",
+  },
+  {
+    label: "APS E AMBULATÓRIO",
+    link: "https://www.delftstack.com/pt/howto/css/hide-scrollbar-css/#defina-scrollbar-width-como-none-para-ocultar-a-barra-de-rolagem-em-css",
+  },
+  {
+    label: "CAPS E REUE",
+    link: "https://www.delftstack.com/howto/node.js/",
+  },
+  {
+    label: "RAPS E ATENÇÃO HOSPITALAR",
+    link: "https://www.delftstack.com/howto/typescript/",
+  },
+  {
+    label: "RAPS E ATENÇÃO HOSPITALAR",
+    link: "https://developer.mozilla.org/pt-BR/docs/Web/CSS/overflow",
+  },
 
+  ,
+];
+const props = {
+  list: tabs,
+  title: "Cuidado compartilhado de Saúde Mental entre as redes de saúde",
+};
 export default {
   title: "Componentes/PanelSelector",
   component: PanelSelector,
-}
+};
 
-
-export const Default = () =>{
-  return(
-   <PanelSelector list={tabs}/>
-  )
-}
+export const Default = () => {
+  return (
+    <PanelSelector
+      list={tabs}
+      title="Cuidado compartilhado de Saúde Mental entre as redes de saúde"
+    />
+  );
+};
