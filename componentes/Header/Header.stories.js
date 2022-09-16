caaebfba64d90221777d63905c0edb1037366daa
vinnidias <vinicius.3991@gmail.com>
2022-09-16 01:22:56 -0300

    teste do header default(sem tema) e com tema AGP

--- componentes/Header/Header.stories.js
+++ componentes/Header/Header.stories.js
@@ -1,22 +1,31 @@
 import React from 'react'
-import {Header} from './index'
+import { Header } from './index'
 
 export default {
   title: "Componentes/Header",
   component: Header,
-  argTypes: { label: { control: 'text' }}
+  argTypes: { label: { control: 'text' } }
 }
 
 export const Default = () => {
   return <Header
-  titulo = "Consultoria para o seu município"
-  tituloDestaque = " 100% gratuita"
-  texto = "Queremos ajudar você e seu município e temos um time preparado para isso. Você pode marcar uma consultoria ou tirar suas dúvidas de forma rápida e descomplicada. E o melhor: tudo de forma gratuita." 
-  botao = { { label: "Inscreva-se para a consultoria", url: "#formulario"}}
-  chamada = {{ label: "CONHEÇA NOSSAS SOLUÇÕES >", url: "" }}
-  theme="ColorAGP"
+    titulo="Consultoria para o seu município"
+    tituloDestaque=" 100% gratuita"
+    texto="Queremos ajudar você e seu município e temos um time preparado para isso. Você pode marcar uma consultoria ou tirar suas dúvidas de forma rápida e descomplicada. E o melhor: tudo de forma gratuita."
+    botao={{ label: "Inscreva-se para a consultoria", url: "#formulario" }}
+    chamada={{ label: "CONHEÇA NOSSAS SOLUÇÕES >", url: "" }}
   />
+}
 
+export const AGP = ()=> {
+  return <Header
+    titulo="Consultoria para o seu município"
+    tituloDestaque=" 100% gratuita"
+    texto="Queremos ajudar você e seu município e temos um time preparado para isso. Você pode marcar uma consultoria ou tirar suas dúvidas de forma rápida e descomplicada. E o melhor: tudo de forma gratuita."
+    botao={{ label: "Inscreva-se para a consultoria", url: "#formulario" }}
+    chamada={{ label: "CONHEÇA NOSSAS SOLUÇÕES >", url: "" }}
+    theme="ColorAGP"
+  />
 }
 
 
