e06f7c6d637bd63a1f4bd1e30ffebf1196b73096
vinnidias <vinicius.3991@gmail.com>
2022-09-16 00:22:10 -0300

    testando tema agp

--- componentes/Header/Header.stories.js
+++ componentes/Header/Header.stories.js
@@ -13,7 +13,8 @@ export const Default = () => {
   tituloDestaque = " 100% gratuita"
   texto = "Queremos ajudar você e seu município e temos um time preparado para isso. Você pode marcar uma consultoria ou tirar suas dúvidas de forma rápida e descomplicada. E o melhor: tudo de forma gratuita." 
   botao = { { label: "Inscreva-se para a consultoria", url: "#formulario"}}
-  chamada = {{ label: "", url: "" }}
+  chamada = {{ label: "CONHEÇA NOSSAS SOLUÇÕES >", url: "" }}
+  theme="ColorAGP"
   />
 
 }
