a8c9b5ade82812ab984ac1d6739afc496a8aef3c
vinnidias <vinicius.3991@gmail.com>
2022-09-16 01:11:04 -0300

    teste footer

--- componentes/Footer/Footer.stories.js
+++ componentes/Footer/Footer.stories.js
@@ -1,32 +1,56 @@
 import React from 'react'
-import { Footer} from './index'
+import { Footer } from './index'
 
 export default {
     title: "Componentes/Footer",
     component: Footer,
 }
-  
-export const Default = () =>{
-  return(
-    <Footer 
-        theme={{
-            logoProjeto : "https://media.graphassets.com/Kal4aulRmYkqd0L6RBAd",
-            cor : "ColorIP"
-        }}
-        menu={
-            [
-                { label: "A Impulso Gov", url: "/impulsogov" },
-                { label: "O Previne Brasil", url: "/previnebrasil" },
-                { label: "Análise", url: "" },
-                { label: "Consultoria", url: "/consultoria" }
-            ]
-        }
-        subtitles = {[
-            { label: "Indicadores de Desempenho", url:"/indicadores" },
-            { label: "Capitação Ponderada", url:"/capitacao" },
-            { label: "Ações Estratégicas", url: "/acoes-estrategicas" },
-        ]}
-    />
-  )
+
+export const Default = () => {
+    return (
+        <Footer
+            contactCopyright={{
+                email: "contato@impulsogov.org",
+                copyright: "© 2021 Impulso"
+            }}
+            theme={{
+                logoProjeto: "https://media.graphassets.com/Kal4aulRmYkqd0L6RBAd",
+                cor: "Black"
+            }}
+            menu={
+                [
+                    { label: "A Impulso Gov", url: "/impulsogov" },
+                    { label: "O Previne Brasil", url: "/previnebrasil" },
+                    { label: "Análise", url: "" },
+                    { label: "Consultoria", url: "/consultoria" }
+                ]
+            }
+            subtitles={[
+                { label: "Indicadores de Desempenho", url: "/indicadores" },
+                { label: "Capitação Ponderada", url: "/capitacao" },
+                { label: "Ações Estratégicas", url: "/acoes-estrategicas" },
+            ]}
+            links={[
+                {
+                    label: "Sobre", url: "/sobre"
+                },
+                {
+                    label: "Competências e Soluções", url: "/competencias"
+                }
+            ]}
+
+            address={{
+                first: "Rua Aracaju 100 - Vila Madalena,",
+                second: "São Paulo - SP, 01010-100"
+            }}
+
+            socialMediaURLs={[
+                {
+                    url: "http://instagran.com",
+                    logo: ""
+                }
+            ]}
+        />
+    )
 }
 
