f0835cee3f5584807d3ab3d870df93aa17fc649d
vinnidias <vinicius.3991@gmail.com>
2022-09-22 21:08:33 -0300

    componente toggle text

---
+++ componentes/ToggleText/ToggleText.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import styles from "./ToggleText.module.css"
+
+const ToggleText = () => {
+  return (
+    <div className={styles.ToggleTextContainer}>
+      <p>
+        Entenda como interpretar os indicadores Impulso
+      </p>
+    </div>
+  )
+};
+
+export { ToggleText }
