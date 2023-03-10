dd50aad49c3c9d8883ef97590015a9b1ee00fd39
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-03-09 22:58:22 -0300

    v1.0.10

--- componentes/ModulosTrilha/MateriaisComplementares.stories.js
+++ componentes/ModulosTrilha/MateriaisComplementares.stories.js
@@ -9,9 +9,6 @@ const Template = (args) => <MateriaisComplementares {...args}/>
 
 export const Desktop = Template.bind({});
 Desktop.args={
-      tituloTrilha: "Hipertensão e Diabetes",
-      botaoVoltar: {label: "VOLTAR",url:"/"},
-      botaoWhatsapp: {label: "ENTRAR NO GRUPO DO WHATSAPP",url:"/"},
       modulos:[
             {id: 0, tipo:"MATERIAIS", titulo: "Introdução aos indicadores de Hipertensão e Diabetes na APS",liberado:true,concluido:true},
             {id: 1, tipo:"MATERIAIS", titulo: "Introdução aos indicadores de Hipertensão e Diabetes na APS",liberado:true,concluido:true},
@@ -26,6 +23,5 @@ Desktop.args={
             {id: 2, moduloID: 2, formato:"QUIZ", titulo:"Quizz de avaliação desses conteúdos", concluido: false, link:"/"}
       ],
       ultimoModulo: 0,
-      mobile: false,
-      checkSobre: true
+      mobile: false
 }
