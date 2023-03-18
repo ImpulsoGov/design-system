86742e8c60de658160a258cd905e5049f1f8e4fd
Taina Santana <tainnaps@gmail.com>
2023-03-17 22:30:20 -0300

    corrige indentação e remove prop coluna

--- componentes/GraficoGrid/GraficoGrid.jsx
+++ componentes/GraficoGrid/GraficoGrid.jsx
@@ -8,41 +8,41 @@ import styles from "./GraficoGrid.module.css";
 const GraficoGrid = ({ info, cards, proporcao }) => {
   return (
     <div className={styles.GraficoGrid}>
-        {info && (
-            <GraficoInfo
-                titulo={info.titulo}
-                descricao={info.descricao}
-                fonte={info.fonte}
-                link={info.link}
-                tooltip={info.tooltip}
-            />
-        )}
+      {info && (
+        <GraficoInfo
+          titulo={info.titulo}
+          descricao={info.descricao}
+          fonte={info.fonte}
+          link={info.link}
+          tooltip={info.tooltip}
+        />
+      )}
 
-        <div className={styles.GraficoGridCards}>
-            {cards.map((card) => (
-                <div className={styles[`Proporcao${proporcao}`]}>
-                    <CardInfo
-                        titulo={card.titulo}
-                        indicador={card.indicador}
-                        indice={card.indice}
-                        indiceDescricao={card.indiceDescricao}
-                        link={card.link}
-                        fonte={card.fonte}
-                        tooltip={card.tooltip}
-                    />
-                </div>
-            ))}
-        </div>
+      <div className={styles.GraficoGridCards}>
+        {cards.map((card) => (
+          <div className={styles[`Proporcao${proporcao}`]}>
+            <CardInfo
+              titulo={card.titulo}
+              indicador={card.indicador}
+              indice={card.indice}
+              indiceDescricao={card.indiceDescricao}
+              link={card.link}
+              fonte={card.fonte}
+              tooltip={card.tooltip}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
 GraficoGrid.defaultProps = {
-    proporcao: "6-6"
+  proporcao: "6-6"
 }
 
 GraficoGrid.propTypes = {
-    proporcao: PropTypes.string
+  proporcao: PropTypes.string
 }
 
 export { GraficoGrid }
