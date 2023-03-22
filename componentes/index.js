646a7ca778531c453952bcbcd164c91f2029aaab
Taina Santana <tainnaps@gmail.com>
2023-03-22 11:38:26 -0300

    atualiza exports de componentes

--- componentes/index.js
+++ componentes/index.js
@@ -10,24 +10,24 @@ export { CardLarge,CardLargeGrid, CardLargeGridInicioSM } from "./CardLarge/Card
 export { CardTrilha} from "./CardTrilha/CardTrilha";
 export { Content3Col } from "./Content3Col/Content3Col";
 export { ConteudoBlog } from "./ConteudoBlog/ConteudoBlog";
-export { ConteudoTrilha } from "./ConteudoTrilha/ConteudoTrilha"
+export { ConteudoTrilha } from "./ConteudoTrilha/ConteudoTrilha";
 export { Footer } from "./Footer/Footer";
 export { FormConsultoria } from "./FormConsultoria/FormConsultoria";
 export { GraficoGrid } from "./GraficoGrid/GraficoGrid";
 export { GraficoInfo } from "./GraficoInfo/GraficoInfo";
-export { Greeting } from "./Greeting/Greeting"
+export { Greeting } from "./Greeting/Greeting";
 export { Header } from "./Header/Header";
 export { HomeBanner } from "./HomeBanner/HomeBanner";
 export { IFrame } from "./IFrame/IFrame"
 export { ImagemFundo } from "./ImagemFundo/ImagemFundo";
-export { InfoTab } from "./InfoTab/InfoTab"
+export { InfoTab } from "./InfoTab/InfoTab";
 export { ListaArtigos } from "./ListaArtigos/ListaArtigos";
 export { Login } from "./Login/Login";
 export { Modal } from "./Modal/Modal";
 export { ModalLogged } from "./ModalLogged/ModalLogged";
 export { ModulosTrilha, MateriaisComplementares } from "./ModulosTrilha/ModulosTrilha";
 export { NavBar } from "./NavBar/NavBar";
-export { PanelSelector } from "./PanelSelector/PanelSelector"
+export { PanelSelector } from "./PanelSelector/PanelSelector";
 export { Parcerias } from "./Parcerias/Parcerias";
 export { ParceriasTexto } from "./ParceriasTexto/ParceriasTexto";
 export { PreviewArtigo } from "./PreviewArtigo/PreviewArtigo";
