6a8f4e9ece58584a085717b4a402f29ccb7d614c
Taina Santana <tainnaps@gmail.com>
2023-03-22 14:59:00 -0300

    atualiza stories card tipo B

--- componentes/CardInfo/CardInfoB.stories.js
+++ componentes/CardInfo/CardInfoB.stories.js
@@ -3,7 +3,48 @@ import { CardInfoTipoB } from './index';
 export default {
   title: "Componentes/CardInfoTipoB",
   component: CardInfoTipoB,
-  argTypes: {}
+  argTypes: {
+    titulo: {
+      name: "titulo",
+      description: "Titulo do card *string*"
+    },
+    tooltip: {
+      name: "tooltip",
+      description: "Mensagem do tooltip do card *string*"
+    },
+    indicador: {
+      name: "indicador",
+      description: "Valor do indicador do card *number*"
+    },
+    indicadorDescricao: {
+      name: "indicadorDescricao",
+      description: "Descrição do indicador do card *string*"
+    },
+    indicadorSimbolo: {
+      name: "indicadorSimbolo",
+      description: "Símbolo associado ao indicador *string* \n\n Exemplo: %"
+    },
+    indicadorTotal: {
+      name: "indicadorTotal",
+      description: "Valor total do indicador exibido no card para calcular porcentagem exibida no gráfico *number*"
+    },
+    indice: {
+      name: "indice",
+      description: "Índice do indicador do card *number*"
+    },
+    indiceDescricao: {
+      name: "indiceDescricao",
+      description: "Descrição do índice do card *string*"
+    },
+    indiceSimbolo: {
+      name: "indiceSimbolo",
+      description: "Símbolo associado ao índice *string* \n\n Exemplo: %"
+    },
+    descricao: {
+      name: "descricao",
+      description: "Descrição do card *string*"
+    },
+  }
 }
 
 const Template = (args) => <CardInfoTipoB {...args}/>
