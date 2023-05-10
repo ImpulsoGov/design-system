bdd26b8a4718368b87fac62bc99ad1128fe0c1e1
Taina Santana <tainnaps@gmail.com>
2023-05-09 22:47:22 -0300

    add spinner após login

--- componentes/Login/Login.jsx
+++ componentes/Login/Login.jsx
@@ -3,74 +3,83 @@ import { useState } from "react";
 import style from "./Login.module.css"
 import { ButtonLightSubmit } from "../ButtonLight";
 import cx from 'classnames';
+import { Spinner } from "../Spinner";
 
 const Login = (props)=>{
     const [senha, setSenha] = useState("");
     const [mail, setMail] = useState("");
     const [mostraSenha, setMostraSenha] = useState(false);
     const [resposta,setResposta] = useState();
+    const [loading, setLoading] = useState(false);
     const color = props.botaoPrincipal.theme ? props.botaoPrincipal.theme : 'ColorIP';
 
     return(
-        <div className={style.LoginConteiner}>
-            <div className={style.LoginTitulo}>{props.titulo}</div>
-            <div className={style.LoginCampos}>
-                <input 
-                    className={style.LoginCampo} 
-                    type="text"
-                    placeholder="E-mail"
-                    value={mail}
-                    onChange={(e) => {setMail(e.target.value);}}
-                />
-                <div className={style.InputSenhaContainer}>
-                    <input
-                        className={style.LoginCampo}
-                        type={ mostraSenha ? "text" : "password" }
-                        placeholder="Senha"
-                        value={senha}
-                        onChange={(e) => {setSenha(e.target.value);}}
-                    />
+        <>
+            {loading
+                ? <Spinner theme={color} height={"50vh"} />
+                : (
+                    <div className={style.LoginConteiner}>
+                        <div className={style.LoginTitulo}>{props.titulo}</div>
+                        <div className={style.LoginCampos}>
+                            <input 
+                                className={style.LoginCampo} 
+                                type="text"
+                                placeholder="E-mail"
+                                value={mail}
+                                onChange={(e) => {setMail(e.target.value);}}
+                            />
+                            <div className={style.InputSenhaContainer}>
+                                <input
+                                    className={style.LoginCampo}
+                                    type={ mostraSenha ? "text" : "password" }
+                                    placeholder="Senha"
+                                    value={senha}
+                                    onChange={(e) => {setSenha(e.target.value);}}
+                                />
 
-                    <button
-                        className={style.BotaoMostraSenha}
-                        onClick={() => setMostraSenha(!mostraSenha)}
-                    >
-                        <img
-                            className={style.IconeMostraSenha}
-                            src={ mostraSenha ? "https://media.graphassets.com/KQptzqZRo2anp1Gdm0Wg" : "https://media.graphassets.com/wQYJXFzUSpCMUMc6xp5J" }
-                            alt="eye"
-                        />
-                    </button>
-                </div>
-                <div 
-                    className={style.LoginEsqueciMinhaSenha}
-                    onClick={()=>props.showEsqueciSenha(true)}
-                >
-                    Esqueceu sua senha?
-                </div>
-                {resposta && <div className={style.LoginResposta}>{resposta}</div>
-}
-            </div>
-            <div className={style.LoginCampoButton}>
-                <button 
-                    className={
-                        (mail.length>0 && senha.length>0)?
-                        cx(style.LoginButton, style[`${color}`]):
-                        style.LoginButtonInativo
-                    }
-                    onClick={() => {
-                        if (mail.length>0 && senha.length>0){
-                            props.botaoPrincipal.submit(true)
-                            props.validacao(setResposta,props.validarCredencial,props.entrar,mail,senha)
-                        }
-                    }}
-                >{props.botaoPrincipal.label.toUpperCase()}</button>
-                <ButtonLightSubmit
-                    label={props.botaoSecundario.label}
-                    submit={props.botaoSecundario.submit}
-                />
-            </div>
-        </div>
+                                <button
+                                    className={style.BotaoMostraSenha}
+                                    onClick={() => setMostraSenha(!mostraSenha)}
+                                >
+                                    <img
+                                        className={style.IconeMostraSenha}
+                                        src={ mostraSenha ? "https://media.graphassets.com/KQptzqZRo2anp1Gdm0Wg" : "https://media.graphassets.com/wQYJXFzUSpCMUMc6xp5J" }
+                                        alt="eye"
+                                    />
+                                </button>
+                            </div>
+                            <div 
+                                className={style.LoginEsqueciMinhaSenha}
+                                onClick={()=>props.showEsqueciSenha(true)}
+                            >
+                                Esqueceu sua senha?
+                            </div>
+                            {resposta && <div className={style.LoginResposta}>{resposta}</div>}
+                        </div>
+                        <div className={style.LoginCampoButton}>
+                            <button 
+                                className={
+                                    (mail.length>0 && senha.length>0)?
+                                    cx(style.LoginButton, style[`${color}`]):
+                                    style.LoginButtonInativo
+                                }
+                                onClick={() => {
+                                    if (mail.length>0 && senha.length>0){
+                                        setLoading(true);
+                                        props.botaoPrincipal.submit(true);
+                                        props.validacao(setResposta,props.validarCredencial,props.entrar,mail,senha, setLoading);
+                                    }
+                                }}
+                            >{props.botaoPrincipal.label.toUpperCase()}</button>
+                            <ButtonLightSubmit
+                                label={props.botaoSecundario.label}
+                                submit={props.botaoSecundario.submit}
+                            />
+                        </div>
+                    </div>
+                )
+            }
+        </>
     )
 }
 
