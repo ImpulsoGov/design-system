0518cc3005d8a877d7046c73de832a16ca5a824f
dlopes14 <donylopes@gmail.com>
2023-06-19 17:34:47 -0300

    remove echarts import

--- componentes/GraficoBuscaAtiva/GraficoBuscaAtiva.jsx
+++ componentes/GraficoBuscaAtiva/GraficoBuscaAtiva.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import style from "./GraficoBuscaAtiva.module.css"
 import ReactEcharts from 'echarts-for-react';
-import echarts from 'echarts/lib/echarts';
 const GraficoBuscaAtiva = ({dataBarra,dataRosca})=>{
     return (
         <div className={style.GraficoBuscaAtiva}>
@@ -9,14 +8,12 @@ const GraficoBuscaAtiva = ({dataBarra,dataRosca})=>{
                 <ReactEcharts
                     option={dataBarra}
                     style={{ height: '600px', width: '100%' }}
-                    echarts={echarts}
                 />
             </div>
             <div className={style.Rosca}>
                 <ReactEcharts
                     option={dataRosca}
                     style={{ height: '600px', width: '100%' }}
-                    echarts={echarts}
                 />
             </div>
         </div>
