5452afe8f9a27402ba4299d3b52e6646047c79b6
João Vittor Souza <80353966+jvns2010@users.noreply.github.com>
2023-07-19 20:23:18 -0300

    resolvido imagens desalinhadas dependendo do tamanho do texto

--- componentes/PreviewArtigo/PreviewArtigo.stories.js
+++ componentes/PreviewArtigo/PreviewArtigo.stories.js
@@ -32,6 +32,7 @@ export default {
     },
 };
 
+
 const Template = (args) => <PreviewArtigo {...args}/>
 
 export const Default = Template.bind({});
@@ -43,5 +44,16 @@ Default.args={
     autor : {avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w", nome: "IMPULSO GOV", data:"2022-08-24T00:42:00+00:00"},
     imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
 }
+
+// New instance of PreviewArtigo below the original one
+export const SecondInstance = Template.bind({});
+SecondInstance.args = {
+    id : "cl6v73chm2xbn0blwom0s8o382349234ueja",
+    tag : "Capitação Ponderada",
+    titulo : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    texto : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat velit nec erat aliquam fringilla. Nam vestibulum eu neque non convallis. Pellentesque elementum dignissim ante. Nulla lacinia enim lacinia erat cursus scelerisque erata nam vestibulum eu neque non convallis.",
+    autor : {avatar: "https://media.graphassets.com/53A911TkTVeKdKeLllnS?_gl=1*kapmz3*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjQxNzcuNjAuMC4w", nome: "IMPULSO GOV", data:"2022-08-24T00:42:00+00:00"},
+    imagem : "https://media.graphassets.com/W10ybMgASFuK4fdT84GM?_gl=1*140dhjr*_ga*MzY0MzkwNjMwLjE2NTg1OTU1NjU.*_ga_G6FYGSYGZ4*MTY2MTEyNDA2NC4zOS4xLjE2NjExMjUzODIuNDcuMC4w"
+};
 
 
