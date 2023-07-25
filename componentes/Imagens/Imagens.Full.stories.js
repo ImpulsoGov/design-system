a483a643ccdb81f955738837ac3f0ae9a6b7e64a
gabriellearruda <gabriellearrudac@gmail.com>
2023-07-25 18:37:23 -0300

    Adiciona imagem redonda

--- componentes/Imagens/Imagens.Full.stories.js
+++ componentes/Imagens/Imagens.Full.stories.js
@@ -13,23 +13,20 @@ export default {
 };
 
 const Template = (args) => <ImagensFull {...args}/>
-
 export const Default = Template.bind({});
 Default.args={
   imagem : 'https://media.graphassets.com/wOdRiF1HQ2azIVk9iCxI'
 }
 
 const Template2 = (args) => <ImagensFull2 {...args}/>
-
 export const Default2 = Template2.bind({});
-Default.args={
+Default2.args={
   imagem : 'https://media.graphassets.com/wOdRiF1HQ2azIVk9iCxI'
 }
 
 const Template3 = (args) => <ImagensFull3 {...args}/>
-
 export const Default3 = Template3.bind({});
-Default.args={
+Default3.args={
   imagem : 'https://media.graphassets.com/wOdRiF1HQ2azIVk9iCxI'
 }
 
