1493bd383002dc5795e00699adb9730064c7e5ea
dlopes14 <donylopes@gmail.com>
2023-11-30 13:00:37 -0300

    altera posicao sub-abas do seletor de paineis

--- componentes/PanelSelector/PanelSelector.jsx
+++ componentes/PanelSelector/PanelSelector.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { IFrame } from "../IFrame/IFrame";
 import cx from "classnames";
 import style from "./PanelSelector.module.css";
 
 const PanelSelector = ({
-  panel,
-  initialTitle,
   titles,
   list,
   conteudo,
@@ -13,6 +11,36 @@ const PanelSelector = ({
   components,
   states
 }) => {
+  const [distanciaSubAba,setDistanciaSubAba] = useState(0)
+  const titleRefs = titles.map(() => useRef(null));
+  useEffect(()=>{
+    states.setActiveTabIndex(0)
+  },[states.activeTitleTabIndex])
+  const PanelSelectorContainerPosition = {
+    position : "relative",
+    left : `${distanciaSubAba}px`
+  }
+  useEffect(() => {
+    const getDistanceFromLeft = (index) => {
+      const divElement = titleRefs[index].current;
+      if (divElement && index==states.activeTitleTabIndex) {
+        const rect = divElement.getBoundingClientRect();
+        setDistanciaSubAba(rect.left + window.scrollX)
+      }
+    };
+    titleRefs.forEach((ref, index) => {
+      const currentRef = ref.current;
+      if (currentRef) {
+        getDistanceFromLeft(index);
+        window.addEventListener("resize", () => getDistanceFromLeft(index));
+      }
+    });
+    return () => {
+      titleRefs.forEach((ref, index) => {
+        if (ref.current) window.removeEventListener("resize", () => getDistanceFromLeft(index));
+      });
+    };
+  }, [titleRefs, states.activeTitleTabIndex]);
   return (
     <div style={{zIndex:90,width:'100%'}}>
       <div className={style.PanelSelectorMain}>
@@ -23,6 +51,7 @@ const PanelSelector = ({
                 states.setActiveTitleTabIndex(index);
               }}
               key={index}
+              ref={titleRefs[index]}
               className={
                 states.activeTitleTabIndex === index
                   ? style.PanelSelectorTitleButtonSelected
@@ -34,7 +63,7 @@ const PanelSelector = ({
           ))}
         </div>
 
-        <div className={cx(style.PanelSelectorContainerIP,style["PanelSelectorContainerPosition"+states.activeTitleTabIndex.toString()])}>
+        <div className={style.PanelSelectorContainerIP} style={PanelSelectorContainerPosition}>
           {list[states.activeTitleTabIndex].map((item, index) => (
             <div
               onClick={() => {
