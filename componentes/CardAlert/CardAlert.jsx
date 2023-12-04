3378313c28bd1c5640212c17b56cdc042678ec0e
NicoliSte <nickstefan4@gmail.com>
2023-12-04 17:37:32 -0300

    adicionando propriedade de cor no cardAlert

--- componentes/CardAlert/CardAlert.jsx
+++ componentes/CardAlert/CardAlert.jsx
@@ -1,30 +1,34 @@
 import React from "react";
-import style from "./CardAlert.module.css"
+import style from "./CardAlert.module.css";
 import { sanitize } from "../sanitize";
 
-const CardAlert = ({
-    msg,
-    destaque
-})=>{
-    return(
-        <div className={style.CardAlert}>
-            <b>{destaque}</b>
-            {msg}
-        </div>
-    )
-}
-const CardAlertChild = ({
-    msg
-})=>{
-    return(
-        <div 
-            className={style.CardAlertChild}
-            dangerouslySetInnerHTML={{
-                __html: sanitize(msg),
-              }} 
-        >
-        </div>
-    )
-}
+const CardAlert = ({ msg, destaque, backgroundColor }) => {
+  const cardStyle = {
+    backgroundColor: backgroundColor || "#1F1F1F", 
+  };
 
-export {CardAlert,CardAlertChild}
+  return (
+    <div className={style.CardAlert} style={cardStyle}>
+      <b>{destaque}</b>
+      {msg}
+    </div>
+  );
+};
+
+const CardAlertChild = ({ msg, backgroundColor }) => {
+  const cardChildStyle = {
+    backgroundColor: backgroundColor || "#1F1F1F", 
+  };
+
+  return (
+    <div
+      className={style.CardAlertChild}
+      style={cardChildStyle}
+      dangerouslySetInnerHTML={{
+        __html: sanitize(msg),
+      }}
+    ></div>
+  );
+};
+
+export { CardAlert, CardAlertChild };
