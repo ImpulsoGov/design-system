175cc98a3137e083b5bcdcaa4c85c07dcdf82e36
Taina Santana <tainnaps@gmail.com>
2024-02-15 11:57:38 -0300

    Add botão auxiliar no ModalLogged

--- componentes/ModalLogged/ModalLogged.stories.js
+++ componentes/ModalLogged/ModalLogged.stories.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { ModalLogged } from './index'
+import React from 'react';
+import { ModalLogged } from './index';
 
 export default {
     title: "Componentes/ModalLogged",
@@ -29,9 +29,19 @@ export default {
             name:'logout',
             description:'Função para logout do usuário *function*'
         },
+        botaoAuxiliar: {
+            name: 'botaoAuxiliar',
+            description: 'Objeto opcional com as propriedades do botão auxiliar do modal *object* \n\n **- label:** rótulo do botão auxiliar *string* \n\n **- handelClick:** função executado quando o botão auxiliar é clicado *function*',
+        }
     },
 };
-const sout = ()=> console.log("logout")
+
+const sout = ()=> console.log("logout");
+const botaoAuxiliar = {
+    label: "GESTÃO DE USUÁRIOS",
+    handelClick: () => console.log("Click botão auxiliar")
+};
+
 const Template = (args) => <ModalLogged {...args}/>
 export const Default = Template.bind({});
 Default.args={
@@ -40,5 +50,16 @@ Default.args={
     cargo : "Coordenadora APS",
     equipe : "00003287",
     button : {label:"sair",link:""},
-    logout : sout
-}
+    logout : sout,
+};
+
+export const ComBotaoAuxiliar = Template.bind({});
+ComBotaoAuxiliar.args={
+    nome : "Camila Alves",
+    label : "C",
+    cargo : "Coordenadora APS",
+    equipe : "00003287",
+    button : {label:"sair",link:""},
+    logout : sout,
+    botaoAuxiliar
+};
