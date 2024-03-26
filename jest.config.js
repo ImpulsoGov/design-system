8e52c754bffbe8c9405ef4d645d7539846a5b76c
Tainá Santana <tainnaps@gmail.com>
2024-03-26 13:00:11 -0300

    Instalação e setup dependências de teste

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const nextJest = require('next/jest');
+
+/** @type {import('jest').Config} */
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'componentes/**/*.{js,jsx}',
+    '!componentes/**/*.stories.js',
+  ],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/docs/',
+  ],
+  coverageProvider: 'babel',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
+module.exports = createJestConfig(config);
