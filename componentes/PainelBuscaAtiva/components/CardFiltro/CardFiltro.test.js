a7c9c128e17172c9de30f6e4a2db8401d41a6a4d
Tainá Santana <tainnaps@gmail.com>
2024-04-10 16:26:43 -0300

    Refatora componente CardFiltro

--- componentes/PainelBuscaAtiva/components/CardFiltro/CardFiltro.test.js
+++ componentes/PainelBuscaAtiva/components/CardFiltro/CardFiltro.test.js
@@ -4,48 +4,45 @@ import userEvent from '@testing-library/user-event';
 import { CardFiltro } from './CardFiltro';
 
 const COMPONENT = 'CardFiltro';
-const setOrdenar = jest.fn();
 const scenarios = [
   {
     label: 'NOME DO ACS DE A-Z',
-    setOrdenar,
-    ordenar: '1',
-    ID: { 'NOME DO ACS DE A-Z': 'acs_nome' }
+    isSelected: false,
+    sortProperty: 'acs_nome',
+    setOrdenar: jest.fn(),
+  },
+  {
+    label: 'NOME DO ACS DE A-Z',
+    isSelected: true,
+    sortProperty: 'acs_nome',
+    setOrdenar: jest.fn(),
   },
 ];
 
 describe(`Componente: ${COMPONENT}`, () => {
   it('deve renderizar corretamente', () => {
-    render(
-      <CardFiltro
-        {...scenarios[0]}
-      />
-    );
+    render(<CardFiltro { ...scenarios[0] } />);
+
     const component = screen.getByTestId(COMPONENT);
+
     expect(component).toMatchSnapshot();
   });
 
-  describe('Ao clicar no componente', () => {
-    it('deve renderizar o card contornado', async () => {
-      render(
-        <CardFiltro
-          {...scenarios[0]}
-        />
-      );
-      const component = screen.getByTestId(COMPONENT);
-      await userEvent.click(component);
-      expect(screen.getByTestId(COMPONENT)).toMatchSnapshot();
-    });
+  it('deve exibir o card com borda quando ele está selecionado', () => {
+    render(<CardFiltro { ...scenarios[1] } />);
 
-    it('deve chamar setOrdenar com o valor correto', async () => {
-      render(
-        <CardFiltro
-          {...scenarios[0]}
-        />
-      );
-      const component = screen.getByTestId(COMPONENT);
-      await userEvent.click(component);
-      expect(setOrdenar).toHaveBeenCalledWith('acs_nome');
-    });
+    const component = screen.getByTestId(COMPONENT);
+
+    expect(component.style).toHaveProperty('border');
+  });
+
+  it('deve chamar setOrdenar com o valor correto após ser clicado', async () => {
+    const user = userEvent.setup();
+    render(<CardFiltro { ...scenarios[0] } />);
+
+    const component = screen.getByTestId(COMPONENT);
+    await user.click(component);
+
+    expect(scenarios[0].setOrdenar).toHaveBeenCalledWith('acs_nome');
   });
 });
