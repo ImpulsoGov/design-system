092d613445fffbc830725947c1fdb2e585726e5c
dlopes14 <donylopes@gmail.com>
2024-08-06 13:30:33 -0300

    ajuste largura modo retrato para gestantes

--- componentes/PainelBuscaAtiva/__data__/stories/gestantes/indicador_3/larguraColunasGestantesIndicador3.js
+++ componentes/PainelBuscaAtiva/__data__/stories/gestantes/indicador_3/larguraColunasGestantesIndicador3.js
@@ -1,10 +1,10 @@
 export const larguraColunasGestantesIndicador3Retrato = {
-  'cidadao_nome': "320px",
+  'cidadao_nome': "180px",
   'gestacao_data_dpp': "120px",
   'id_atendimento_odontologico': "120px",
   'id_registro_parto': "120px",
-  'equipe_nome': "200px",
-  'acs_nome': "320px",
+  'equipe_nome': "140px",
+  'acs_nome': "180px",
 };
 
 export const larguraColunasGestantesIndicador3Paisagem = {
--- componentes/PainelBuscaAtiva/__data__/stories/gestantes/semDum/larguraColunasGestantesSemDum.js
+++ componentes/PainelBuscaAtiva/__data__/stories/gestantes/semDum/larguraColunasGestantesSemDum.js
@@ -1,15 +1,15 @@
 export const larguraColunasSemDumRetrato = {
-  'cidadao_nome': "175px",
+  'cidadao_nome': "125px",
   'cidadao_cpf_dt_nascimento': "80px",
-  'gestacao_data_dpp': "80px",
-  'gestacao_idade_gestacional_primeiro_atendimento': "80px",
-  'gestacao_idade_gestacional_atual': "80px",
-  'consultas_pre_natal_validas': "120px",
-  'consulta_prenatal_ultima_data': "80px",
-  'id_exame_hiv_sifilis': "80px",
-  'id_atendimento_odontologico': "80px",
-  'equipe_nome': "170px",
-  'acs_nome': "175px",
+  'gestacao_data_dpp': "130px",
+  'gestacao_idade_gestacional_primeiro_atendimento': "60px",
+  'gestacao_idade_gestacional_atual': "60px",
+  'consultas_pre_natal_validas': "60px",
+  'consulta_prenatal_ultima_data': "60px",
+  'id_exame_hiv_sifilis': "60px",
+  'id_atendimento_odontologico': "60px",
+  'equipe_nome': "120px",
+  'acs_nome': "125px",
 };
 
 export const larguraColunasSemDumPaisagem = {
